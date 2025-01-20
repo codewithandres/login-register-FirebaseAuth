eec1dc769ad6f944aa11ee1e7ae7e6e1f3fa93fe
Dize dev <axiozweb2022@gmail.com>
2025-01-19 22:31:35 -0500

    Mejora la validación de correo electrónico lanzando un error en caso de formato incorrecto

--- src/validation/validatyeEmail.ts
+++ src/validation/validatyeEmail.ts
@@ -1,14 +1,15 @@
 
+
 const validateEmail = (inputEmail: HTMLInputElement): boolean => {
     // some code
     const expRegEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     if (expRegEmail.test(inputEmail.value.trim())) {
-        inputEmail.closest('.form-group')?.classList.remove('error')
+        inputEmail.closest('.form-group')?.classList.remove('error');
         return true;
     } else {
-        inputEmail.closest('.form-group')?.classList.add('error')
-        return false;
+        inputEmail.closest('.form-group')?.classList.add('error');
+        throw new Error('El correo es Incorrecto')
     }
 }
 
