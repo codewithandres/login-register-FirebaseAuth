8bb633593271c85bc63fb7228385958d8d46fe19
Dize dev <axiozweb2022@gmail.com>
2025-01-20 22:08:39 -0500

    Agrega validación de contraseña al formulario de inicio de sesión y mejora la gestión de errores

--- src/formulario.ts
+++ src/formulario.ts
@@ -1,20 +1,36 @@
 
-import { formsingIn } from "./constans";
-import { validateEmail } from "./validation/validatyeEmail";
+import { formsingIn } from "@/constans";
+import { validateEmail } from "@/validation/validatyeEmail";
+import { validatePassword } from "@/validation/validatePassword";
 
 const bottonFormSignIn = document.getElementById('botton-signIn')
 
-formsingIn.addEventListener('keyup', event => {
-    const target = event.target as HTMLInputElement;
+formsingIn.addEventListener('keyup', (event: KeyboardEvent) => {
+
+    const target = event.target as HTMLInputElement
+
 
     if (target.tagName !== 'INPUT') return;
 
-    if (target.id === 'email') return validateEmail(target)
+    if (target.id === 'email') return validateEmail(target);
+
+    if (target.id === 'password') return validatePassword(target)
 })
 
 bottonFormSignIn?.addEventListener('click', event => {
     event.preventDefault();
 
-    if (validateEmail(formsingIn.email)) return;
+    if (validateEmail(formsingIn.email)) {
+        const errorMessage = validateEmail(formsingIn.email);
+        console.log(errorMessage)
+    };
 
+
+    if (validatePassword(formsingIn.password)) {
+        const errorMessage = validatePassword(formsingIn.password);
+        console.log(errorMessage)
+    }
+    const { email, password } = formsingIn;
+
+    console.log(email.value, password.value);
 })
