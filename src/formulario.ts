882b882e2f1a11f745d6e4eefd7f68237f44cc20
Dize dev <axiozweb2022@gmail.com>
2025-01-22 22:50:51 -0500

    Agrega funcionalidad para alternar entre formularios de inicio de sesión y registro

--- src/formulario.ts
+++ src/formulario.ts
@@ -3,8 +3,12 @@ import { formsingIn } from "@/constans";
 import { validateEmail } from "@/validation/validatyeEmail";
 import { validatePassword } from "@/validation/validatePassword";
 
-const bottonFormSignIn = document.getElementById('botton-signIn')
+const bottonFormSignIn = document.getElementById('botton-signIn');
+const containerFormSingUp = document.querySelector('.container-form-register');
+const containerFormSingIn = document.querySelector('.container-form-login');
 
+const linkSingUp = document.querySelector('.form-link a.singUp') as HTMLAnchorElement;
+const linkSingIn = document.querySelector('.form-link a.singIn') as HTMLAnchorElement;
 formsingIn.addEventListener('keyup', (event: KeyboardEvent) => {
 
     const target = event.target as HTMLInputElement
@@ -33,4 +37,21 @@ bottonFormSignIn?.addEventListener('click', event => {
     const { email, password } = formsingIn;
 
     console.log(email.value, password.value);
+})
+
+linkSingUp.addEventListener('click', event => {
+    event.preventDefault();
+
+    containerFormSingIn?.classList.add('hidden');
+    containerFormSingUp?.classList.remove('hidden');
+    containerFormSingUp?.classList.add('animate__animated', 'animate__backInUp');
+
+})
+
+linkSingIn.addEventListener('click', event => {
+    event.preventDefault();
+
+    containerFormSingIn?.classList.remove('hidden');
+    containerFormSingUp?.classList.add('hidden');
+    containerFormSingIn?.classList.add('animate__animated', 'animate__backInDown');
 })
