1ab7fec9a7287045ce1060cfe4eede9962a20941
Dize dev <axiozweb2022@gmail.com>
2025-01-26 14:42:09 -0500

    elimina importaciones innecesarias en main.ts y simplifica la gestión de estilos

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
 // importacion de estilos
 import 'animate.css'
 import '@/css/root.css';
-import '@/css/dashboard.css';
 import '@/css/index.css';
 
 // importacion de componentes
 import '@/formulario'
 import '@/formulario-singUp'
 
-// importacion de funciones de UI
-import { showSlidebar} from '@/UI/sidebarActtion';
-
-const SelectorSidebar = { 
-    toggleId: 'header__togle',
-    sidebarId: 'sidebar',
-    mainId: 'main'
-}
-
-showSlidebar(SelectorSidebar)
