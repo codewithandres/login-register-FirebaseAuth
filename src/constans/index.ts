f8b8dfbd31e4939acba7ce188f2f2915c624d304
Dize dev <axiozweb2022@gmail.com>
2025-01-26 14:42:16 -0500

    refactor index.ts para encapsular elementos del formulario y mejorar la gestión de exportaciones

--- src/constans/index.ts
+++ src/constans/index.ts
@@ -1,4 +1,26 @@
 
-export const formsingIn = document.getElementById('form-sigIn') as HTMLFormElement;
+const formsingIn = document.getElementById('form-sigIn') as HTMLFormElement;
 
-export const formsingUp = document.getElementById('form-sigUp') as HTMLFormElement;
+const formsingUp = document.getElementById('form-sigUp') as HTMLFormElement;
+
+const linkSingUp = document.querySelector('.form-link a.singUp') as HTMLAnchorElement;
+const linkSingIn = document.querySelector('.form-link a.singIn') as HTMLAnchorElement;
+
+const containerFormSingUp = document.querySelector('.container-form-register');
+const containerFormSingIn = document.querySelector('.container-form-login');
+
+const iconEye = document.querySelectorAll('.ri-eye-off-fill') as NodeListOf<HTMLElement>;
+
+export {
+    formsingIn,
+    formsingUp,
+
+
+    linkSingIn,
+    linkSingUp,
+    containerFormSingIn,
+    containerFormSingUp,
+
+    
+    iconEye
+}
