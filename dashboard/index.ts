90f8770951564b8b75f79fa05870608ff0999c70
Dize dev <axiozweb2022@gmail.com>
2025-01-26 23:11:25 -0500

    agrega funcionalidad para gestionar el modo oscuro y la barra lateral en el dashboard

--- dashboard/index.ts
+++ dashboard/index.ts
@@ -2,6 +2,15 @@ import '../src/css/root.css';
 import '../src/css/dashboard.css';
 
 // importacion de funciones de UI
-import '../src/UI/sidebarActtion';
+import '../src/UI/sidebarLink';
+import '../src/UI/darkMode';
+import showSlidebar from '../src/UI/sidebarActtion';
 
-console.log(import.meta.url);
+const SelectorSidebar = {
+    toggleId: 'header__togle',
+    sidebarId: 'sidebar',
+    mainId: 'main'
+}
+
+showSlidebar(SelectorSidebar)
+
