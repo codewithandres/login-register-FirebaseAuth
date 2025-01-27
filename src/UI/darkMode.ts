4b4cc640d8ddf0f4aaf3d9f3ecff0b1784e83a24
Dize dev <axiozweb2022@gmail.com>
2025-01-26 23:11:41 -0500

    agrega funcionalidad para gestionar el modo oscuro en la interfaz de usuario

---
+++ src/UI/darkMode.ts
@@ -0,0 +1,24 @@
+const themeButton = document.getElementById('theme-botton');
+const darkTheme: string = 'dark-theme';
+const iconTheme: string = 'ri-sun-line';
+
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-Icon');
+
+const getCurrentTheme = ( ): string => document.body.classList.contains(darkTheme) ? 'dark' : 'light'; 
+const getCurrentIcon = (): string => themeButton?.classList.contains(iconTheme) ? 'ri-moon-clear-line' : 'ri-sun-line'; 
+
+if (selectedTheme) { 
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme); 
+    themeButton?.classList[selectedIcon === 'ri-moon-clear-line' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton?.addEventListener('click' , ( ) => { 
+    document.body.classList.toggle(darkTheme); 
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-Icon', getCurrentIcon());
+    
+})
