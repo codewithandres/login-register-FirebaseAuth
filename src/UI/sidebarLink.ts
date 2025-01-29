444241f511af8e114ee0aa18da9ac55acc97fe25
Dize dev <axiozweb2022@gmail.com>
2025-01-29 10:37:27 -0500

    refactor: corrige la indentación y el formato en el archivo sidebarLink.ts

--- src/UI/sidebarLink.ts
+++ src/UI/sidebarLink.ts
@@ -1,12 +1,12 @@
 
 
 
-const sidebarLink = document.querySelectorAll('.siderbar__list a'); 
+const sidebarLink = document.querySelectorAll('.siderbar__list a');
 
-const linkColor = (event: MouseEvent) => { 
+const linkColor = (event: MouseEvent) => {
     const target = event.target as HTMLElement;
 
-    sidebarLink.forEach(i => i.classList.remove('active__link')); 
+    sidebarLink.forEach(i => i.classList.remove('active__link'));
 
     target.closest('.sidebar__link')?.classList.add('active__link')
 
