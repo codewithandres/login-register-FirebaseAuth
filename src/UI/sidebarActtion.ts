f79e1266c4877b22e34e8fff7d0c041ac4e3aa2d
Dize dev <axiozweb2022@gmail.com>
2025-01-30 20:39:45 -0500

    agrega el ID del encabezado a la función showSlidebar y mejora la gestión de clases en el sidebar

--- src/UI/sidebarActtion.ts
+++ src/UI/sidebarActtion.ts
@@ -3,24 +3,27 @@ interface showSlidebarPro {
     toggleId: string;
     sidebarId: string;
     mainId: string;
+    headerId: string
 }
 
 
-const showSlidebar = ({ toggleId, sidebarId, mainId }: showSlidebarPro) => {
+const showSlidebar = ({ toggleId, sidebarId, mainId, headerId}: showSlidebarPro) => {
 
     const toggle = document.getElementById(toggleId);
     const sidebar = document.getElementById(sidebarId);
     const main = document.getElementById(mainId);
+    const header = document.getElementById(headerId)
 
 
-    if (!toggle && !sidebar && !main) return;
+    if (!toggle && !sidebar && !main && !header) return;
 
     toggle?.addEventListener('click', () => {
 
         console.log('click')
-        sidebar?.classList.toggle('show-sidebar')
+        sidebar?.classList.toggle('show-sidebar');
 
-        main?.classList.toggle('main-pd')
+        main?.classList.toggle('left-pd'); 
+        header?.classList.toggle('left-pd')
     })
 }
 
