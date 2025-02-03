6d96feb143ed48471e4923c848bcacc300baae6a
Dize dev <axiozweb2022@gmail.com>
2025-02-02 21:54:50 -0500

    refactor: mejora la validación en el formulario de registro

--- src/formulario-singUp.ts
+++ src/formulario-singUp.ts
@@ -1,15 +1,16 @@
-import { formsingUp } from "@/constans";
-import { validateName } from "./validation/validateNames";
-import { validateEmail } from "./validation/validatyeEmail";
-import { validatePassword } from "./validation/validatePassword";
-import { validateConformPassword } from "./validation/validateConformPassword";
+import { formsingUp } from '@/constans';
+import { validateName } from './validation/validateNames';
+import { validateEmail } from './validation/validatyeEmail';
+import { validatePassword } from './validation/validatePassword';
+import { validateConformPassword } from './validation/validateConformPassword';
+import { validateForSingUp } from './validation/validateForm/validateForm-singUp';
 
-
-const bottonFormSingUp = document.querySelector('.botton-signUp') as HTMLButtonElement;
+const bottonFormSingUp = document.querySelector(
+    '.botton-signUp'
+) as HTMLButtonElement;
 
 formsingUp.addEventListener('keyup', (event: KeyboardEvent) => {
     if (!event.target || (event.target as HTMLElement).tagName !== 'INPUT') return;
-
 
     const target = event.target as HTMLInputElement;
 
@@ -19,31 +20,19 @@ formsingUp.addEventListener('keyup', (event: KeyboardEvent) => {
 
     if (target.name === 'password') return validatePassword(target);
 
-    if (target.name === 'RepeatPassword') return validateConformPassword({ inputConfirmPassowrd: target, inputPassowrd: formsingUp.password });
-
-
-})
-
+    if (target.name === 'RepeatPassword')
+        return validateConformPassword({
+            inputConfirmPassowrd: target,
+            inputPassowrd: formsingUp.password,
+        });
+});
 
 bottonFormSingUp.addEventListener('click', event => {
     event.preventDefault();
 
     const { names, email, password, RepeatPassword } = formsingUp;
 
+    const {success, error, data  } = validateForSingUp({names, email, password, RepeatPassword});
 
-    if (!validateName(names)) { 
-        return { succes: false, error: 'Ingresa un nombre valido '} 
-    }
-
-    if (!validateEmail(email)) {
-        return { succes: false, error: 'Ingresa un email valido' }
-    }
-
-    if (!validatePassword(password)) {
-        return { succes: false, error: 'Ingresa una contraseña valida' }
-    }
-
-    if (!validateConformPassword({ inputConfirmPassowrd: RepeatPassword, inputPassowrd: password })) {
-        return { succes: false, error: 'Las contraseñas no coinciden'}
-    }
-})
+    console.log(success , error , data);
+});
