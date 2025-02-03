a56cb182e653126a5735c41806d7742bd5f27f94
Dize dev <axiozweb2022@gmail.com>
2025-02-02 19:09:21 -0500

    elimina importaciones innecesarias en index.ts

--- dashboard/index.ts
+++ dashboard/index.ts
@@ -1,19 +1,3 @@
 import '../src/css/root.css';
-import '../src/css/dashboard.css';
-import '../src/css/searchstyle.css'
-import '../src/css/banner.style.css'
-
 // importacion de funciones de UI
-import '../src/UI/sidebarLink';
-import '../src/UI/darkMode';
-import showSlidebar from '../src/UI/sidebarActtion';
-
-const SelectorSidebar = {
-    toggleId: 'header-toggle',
-    sidebarId: 'sidebar',
-    mainId: 'main',
-    headerId: 'header'
-}
-
-showSlidebar(SelectorSidebar)
 
